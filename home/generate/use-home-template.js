513a02fbd0a7d5a1aa81810a78a46fd65af48e3e
0x0nyx98 <nyxmailbox@proton.me>
2024-11-16 22:27:41 -0500

    woooaaaaa big commit ! fixed some things , moved bio page to its own folder , started new home page subpages !!!! but only one accessible for now >w<

--- home/generate/use-home-template.js
+++ home/generate/use-home-template.js
@@ -1,7 +1,7 @@
 async function gen_template() {
     var pagecontent = document.getElementById("main").innerHTML;
     
-    var fetched = await fetch('generate/home-template.htmlesque');
+    var fetched = await fetch('/home/generate/home-template.htmlesque');
     var pagebase = await fetched.text();
     document.getElementById("bg").innerHTML = pagebase;
     document.getElementById("main").innerHTML = pagecontent;
